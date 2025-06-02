69e4a7038432253ff9597aeca5a10e99a503fa35
Taylor Lagneau <taylorlagneau@macbook-pro-de-taylor.home>
2025-06-01 22:12:31 -0400

    Ajout et correction du modèle user.js

---
+++ linkup_backend_full_mac/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
+
