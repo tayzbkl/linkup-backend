bc07e67853c8cc11cecde63228ea9925a1078690
Taylor Lagneau <taylorlagneau@macbook-pro-de-taylor.home>
2025-06-01 22:41:12 -0400

    Correction du fichier userRoutes.js

--- linkup_backend_full_mac/routes/userRoutes.js
+++ linkup_backend_full_mac/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User'); // Corrigé avec la majuscule
+const User = require('../models/User');
 
 router.post('/', async (req, res) => {
   try {
