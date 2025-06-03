b334545d94bccc14d3a973ecd239a6ed8d4dce4a
Taylor Lagneau <taylorlagneau@macbookpro.home>
2025-06-02 23:20:34 -0400

    Correction du nom de fichier User.js

--- linkup_backend_full_mac/routes/userRoutes.js
+++ linkup_backend_full_mac/routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const user = require('../models/user');
 
 router.post('/', async (req, res) => {
   try {
@@ -16,5 +16,6 @@ router.post('/', async (req, res) => {
 });
 
 module.exports = router;
+
 
 
